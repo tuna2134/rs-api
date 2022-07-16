63954d155d7e71c3dffd9d7be85fa03b73057596
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-16 13:51:57 +0000

    ok

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+use sqlx::mysql::MySqlPool;
+
+use std::env;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
