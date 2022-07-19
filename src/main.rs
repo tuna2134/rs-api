a41d9b802dcc63a70c2a2371940e5f1576353f43
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-19 12:09:49 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn create(pool: web::Data<MySqlPool>) -> impl Responder {
 }
 
 #[get("/create2")]
-async fn create(pool: web::Data<MySqlPool>) -> impl Responder {
+async fn create2(pool: web::Data<MySqlPool>) -> impl Responder {
     let conn = pool.acquire();
     sqlx::query!("CREATE TABLE IF NOT EXISTS Hello(userid BIGINT);")
         .execute(conn)
@@ -25,6 +25,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .service(create)
+            .service(create2)
             .app_data(web::Data::new(pool.clone()))
     })
     .bind(("127.0.0.1", 8080))?
